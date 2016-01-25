3435334b738525db88963dbcf27b4c15132e9959
Ivan Wesley Chua <ivanwesleychua@yahoo.com>
2016-01-25 14:22:12 +0800

    test

--- app/src/main/java/com/thesis/geyoubeta/activity/MapActivity.java
+++ app/src/main/java/com/thesis/geyoubeta/activity/MapActivity.java
@@ -11,12 +11,12 @@ import android.support.v7.app.ActionBarActivity;
 
 import com.thesis.geyoubeta.R;
 
-public class UserInfoEditActivity extends ActionBarActivity {
+public class MapActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_info_edit);
+        setContentView(R.layout.activity_map);
     }
 
 }
